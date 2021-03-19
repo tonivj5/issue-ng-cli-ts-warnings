17ca230b2aefebc0d10f82df98d41d1462f62ee9
tonivj5 <tonivj5@gmail.com>
2021-03-19 10:18:55 +0100

    no warnings :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { identity } from 'rambda';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-issue';
+
+  constructor() {
+    console.log(identity);
+  }
 }
